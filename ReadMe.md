42c729c06954929b3a2ea0d91c50cf42dd604cf3
Jennifer Lee <jenlee.tech@gmail.com>
2018-10-06 13:32:42 -0400

    modified comments and added more commens to readme

--- ReadMe.md
+++ ReadMe.md
@@ -4,10 +4,10 @@
 In this project I was given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. And this is where I continue their work by creating more test suites.  Udacity has provided the repository to clone, in order to start this project.  
 
 ## Motivation:
-Why do we need to test web-base qpplications?  To ensure the quality of the app and note any broken features/elements/components, etc.  And this is where Jasmine comes in - this is an independent, behavior driven development framework for testing Javascript code.  In other words, the framework is used to help us test by "breaking" the Javascript code.  Another analogy is found in Udacity's FEND Nanodegree course, section 3, _Rethinking Tesing_ - "how much load can an airplane's wings take until it breaks?"  
+Why do we need to test web-base qpplications?  To ensure the quality of the app and note any broken features/elements/components, etc after edits/changes.  And this is where Jasmine comes in - this is an independent, behavior driven development framework for testing Javascript code.  In other words, the framework is used to help us test by "breaking" the Javascript code.  Another analogy is found in Udacity's FEND Nanodegree course, section 3, _Rethinking Tesing_ - "how much load can an airplane's wings take until it breaks?"  
+
 
 ## Test Suites that were created:
-
 * _RSS Feeds_
     * test that the allFeeds varible is defined
     * test that the above varible is not empty
@@ -25,6 +25,36 @@ Why do we need to test web-base qpplications?  To ensure the quality of the app 
 * _New Feed Selection_
     * test that each time after a loadFeed function runs, there are are different news feed.
 
+## How do you test these "tests"? :wink:
+1. You could download this repository by clicking the above button that states, "Clone or Download."  You will choose either Download Zip or Open in Desktop.  Put the files in one folder and open index.html in a modern day web browser. 
+
+2. When you open index.html, you will see a newsFeed; towards the bottom of the page, you will see the Jasmine feature and the results of the test suites.  Everything will initially look awesome, green or teal (depending on your screen - no errors).
+
+3. You could test the test suites by editing the allFeeds object in ./js/app.js and save it.  This will make the tests result in "fail."  When you refresh the index.html page, then you are going to see all these crazy lines of error in red/pink.  This will prove the test suites are working!  And they are doing what they are suppose to be doing.
+
+## Prerequisites/Dependencies:
+* A modern day web browser with Javascript enabled.
+
+## Acknowledgements/Resources
+* [Jasmine](https://jasmine.github.io/2.0/introduction.html)
+
+* This repository contains starter code, comments and the original readme file from [Udacity's FeederReader Project](https://github.com/udacity/frontend-nanodegree-feedreader)
+
+* Project Grading Rubric
+
 * Webinar: FeedReader Testing (P4) Sept-23 walk-thru with @Lloan  - [Zoom](https://udenver.zoom.us/recording/play/-1Agy4wDME0_ab_zaNUiWquZOWdb4qQvCJENURKWT4CDtHWqXrE0yI7DSi8kfvm5?continueMode=true)
 
-* Mathew Cranford: [Feeder Reader Walkthrough](https://matthewcranford.com/feed-reader).
+* Mathew Cranford: [Feeder Reader Walkthrough - steps 1-4] (https://matthewcranford.com/feed-reader).
+
+* Markdown cheat sheet from [Github:](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf)
+
+* [Emoji cheat sheet](https://www.webpagefx.com/tools/emoji-cheat-sheet/) for Markdown files
+
+* Udacity's Front End Nanodegree [Style Guides](https://github.com/udacity/frontend-nanodegree-styleguide)
+
+
+## Further comments:
+This project was really cool - to think differently about testing Javascript apps.
+
+## Licensing:
+* Jasmine [license](https://github.com/jasmine/jasmine/blob/master/MIT.LICENSE)
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,50 +1,35 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
+/* We place all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+ * to ensure they don't run until the DOM is ready. Some inital code
+ * was provided by Udacity.
  */
 
- //**The First Test Suite */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+   /* This first test Suite tests to make sure that the allFeeds variable
+    * has been defined and that it is not empty. 
+    */    
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        //this test that allFeeds is defined and not empty
         it('are defined', function() {
           expect(allFeeds).toBeDefined();
           expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
+        /* this test loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('ensure URLs', function(){
+        it('have URLs', function(){
             allFeeds.forEach(element => {
                 expect(element.url).toBeDefined();
                 expect(element.url.length).not.toBe(0);
             });  
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* this test loops through each feed in the allFeeds
+         * object and ensures it has a name defined and it is not empty
          */
-        it('ensure name', function(){
+        it('have names', function(){
             allFeeds.forEach(element => {
                 expect(element.name).toBeDefined();
                 expect(element.name.length).not.toBe(0);
@@ -54,43 +39,42 @@ $(function() {
 
     
     describe('The menu', function(){
-        //**this test that the menu is hidden by default*/
-        it('hides', function(){
+        //this test that the menu is hidden by default
+        it('hides by default', function(){
             const body = document.querySelector('body');
             expect(body.classList.contains('menu-hidden')).toBe(true);
             
         });
           
-        //**this test hides and shows when clicking*/
-        it('hides and then shows upon click', function(){
-            //**DOM elements selected, body and menu icon link */
+        //this test hides and shows when clicking
+        it('hides and appears upon click', function(){
+            //DOM elements selected, body and menu icon link
             const body = document.querySelector('body');
             const menu = document.querySelector('.menu-icon-link');
 
-            //**when a menu is click, we test if the menu is shown*/
+            //when a menu is click, we test if the menu is shown
             menu.click();
             expect(body.classList.contains('menu-hidden')).toBe(false);
-            //**when the menu is click again, the menu will hide*/
+            //when the menu is click again, the menu will hide
             menu.click();
             expect(body.classList.contains('menu-hidden')).toBe(true);
         });
     });
 
-
-        
  
-    //**this test suite tests that the loadFeed function loads and that it has*/
-    //**it has more than one entry*/
-
-    //**beforeEach function is required because loadFeed is asynchronous*/
+    /* this test suite tests that the loadFeed function loads and that it has
+     * it has more than one entry beforeEach function is required because 
+     * loadFeed is asynchronous
+     */
    
     describe('Initial Entries', function(){
         beforeEach(function(done){
-            loadFeed(0, done); //**runs the loadFeed function at index of 0*/
+            loadFeed(0, done); //runs the loadFeed function at index of 0
         });
         
-        //**test there is more than one feed entry in the .feed container after the 
-        //**loadFeed function runs*/
+        /* test there is more than one feed entry in the .feed container after the loadFeed 
+         * function runs
+         */
         it('completes work', function(){
             const feed = document.querySelector('.feed');
             expect(feed.children.length > 0).toBe(true);
@@ -98,32 +82,33 @@ $(function() {
         });
     });
 
-    //**this test suite tests that loadFeed function is generating different feeds
-    //**each time it loads - this test will loads two feeds and compare them*/
+    /* this test suite tests that loadFeed function is generating different feeds each time 
+     * it loads - this test will loads two feeds and compare them
+     */
 
-    //**beforeEach function is required because loadFeed is asynchronous*/
+    //beforeEach function is required because loadFeed is asynchronous
 
     describe('New Feed Selection', function(){
-        const feed = document.querySelector('.feed'); //**selecting element*/
-        let firstFeed, //**variables declared to hold results of the feed*/
+        const feed = document.querySelector('.feed'); //selects the element
+        let firstFeed, //variables declared to hold results of the feed
             secondFeed;
         
       
-        //**this is for the firstFeed with an index of 0*/
+        //this is for the firstFeed with an index of 0
         beforeEach(done => {
             loadFeed(0, function() {
                 firstFeed = feed.innerHTML;
                 done();
             });
          
-        //**this is for the secondFeed with an index of 1*/
+        //this is for the secondFeed with an index of 1
             loadFeed(1, function(){
                 secondFeed = feed.innerHTML;
                 done();
             });
         });
 
-        //**this test both feeds to ensure they are different after running loadFeed function each time*/
+        //this test both feeds to ensure they are different after every run instance of loadFeed
         it('content changes', function(){
             expect(firstFeed !== secondFeed).toBe(true);
         });
